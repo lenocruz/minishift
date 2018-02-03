d52bd54d0b798a030f9e5c1e4910ca274723abf6
itikabc <35884082+itikabc@users.noreply.github.com>
2018-02-03 01:27:52 -0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -20,6 +20,8 @@ touch /tmp/minishiftisdone
 
 systemctl stop script2.service
 systemctl disable script2.service
-rm -f /etc/systemd/system/script2.service
+
 systemctl daemon-reload
 systemctl reset-failed
+
+rm -f /etc/systemd/system/script2.service
